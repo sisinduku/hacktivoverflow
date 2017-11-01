c459e050857779f6782b5b3445bd0cdf630d2925
sisinduku <saptanto.sindu@gmail.com>
2017-11-01 21:08:47 +0700

    initial Dev Setup

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const AuthRoute = require('./routes/AuthRoute');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use('/api/auth', AuthRoute);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Hello from port: 3000');
+});
